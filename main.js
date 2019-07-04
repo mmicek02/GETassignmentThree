42e96dfac76b8e77ecb918386d2f0d99bbf456d8
mmicek02 <micekmichael@gmail.com>
2019-07-03 21:05:58 -0500

    updated HTML and JS for assignment

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 'use strict';
 
 function getDogImage() {
-    let userInput = $('input[type="number"]');
-    let imageNumber = userInput.val();
-    fetch('https://dog.ceo/api/breeds/image/random/' + imageNumber)
+    let userInput = $('input[type="text"]');
+    let breedName = userInput.val();
+    fetch('https://dog.ceo/api/breed/' + breedName + '/images/random')
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => console.log(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`);
+
+  //display the results section
+  console.log('Ready to reveal images!');
+  $('.results').removeClass('hidden');
 }
 
 function watchForm() {
